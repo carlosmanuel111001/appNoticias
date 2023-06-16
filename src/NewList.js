67e7de8021a7a9d51e9c9b8f1ae194167d2aae0f
carlosmanuel111001 <carlos1110fuentes@gmail.com>
2023-06-15 18:05:17 -0600

    Se hacen las instalaciones correspondiente mas las importaciones y se crea el componente NewList

---
+++ src/NewList.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+const NewList = ({navigation}) => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+        params: {
+          country: 'us',
+          apiKey: '4a399700901c4e42a2ac896468192b1d',
+        },
+      });
+      setNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={news}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={styles.newsItem}
+            key={item.id}
+            onPress={() => navigation.navigate('NewsDetail', {newsIte: item})}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id?.toString()}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create();
+
+export default NewList;
+//4a399700901c4e42a2ac896468192b1d
